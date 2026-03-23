3ccd38eb5c8c36e4e7c9b8a8619f1f068fcde722
Abhishek <abhishekkushwaha709194@gmail.com>
2026-03-23 17:41:47 +0530

    add

---
+++ Apna-collage/question1.py
@@ -0,0 +1,11 @@
+# # name ="abhishek"
+# # age = 21
+# # print("my name is", name, "and my age is", age, "years")
+
+# Name ="Abhishek"
+# collage ='Sandip university'
+# print("My name is", Name, "and I study in", collage)
+# _myvar (valid variable name) 
+# 2name not (valid variable name because it starts with number)
+# total_marks (valid variable name)
+# user-name (not valid variable name because it contains hyphen)
